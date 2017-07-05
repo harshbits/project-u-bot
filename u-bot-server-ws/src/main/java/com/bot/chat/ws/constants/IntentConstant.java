fa6be374750709dd9ef591f7d59fdc322859250a
harshbhavsar <harsh.bhavsar@copart.com>
2017-07-05 14:20:27 -0500

    Constant file added

---
+++ u-bot-server-ws/src/main/java/com/bot/chat/ws/constants/IntentConstant.java
@@ -0,0 +1,13 @@
+package com.bot.chat.ws.constants;
+
+public class IntentConstant {
+
+	public static final String WEATHER_INTENT = "weather";
+	
+	public static final String WEATHER_TEMPERATURE_INTENT = "weather.temperature";
+	
+	public static final String WEATHER_ACTIVITY_INTENT = "weather.activity";
+
+	public static final String WEB_INTENT = "web";
+
+}
