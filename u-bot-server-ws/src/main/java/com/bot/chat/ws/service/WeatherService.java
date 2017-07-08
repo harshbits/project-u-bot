fe26b64ffd6d9864283c4168bd6ca952e75aad59
harshbhavsar <harsh.bhavsar@copart.com>
2017-07-08 18:12:07 -0500

    Weather service with tomorrow forecast support

--- u-bot-server-ws/src/main/java/com/bot/chat/ws/service/WeatherService.java
+++ u-bot-server-ws/src/main/java/com/bot/chat/ws/service/WeatherService.java
@@ -2,9 +2,14 @@ package com.bot.chat.ws.service;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import javax.xml.bind.JAXBException;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -44,21 +49,26 @@ public class WeatherService {
 		// Process the request and get city/location
 		String location = processRequestToGetCity(request);
 
+		// Process the request and get requested date
+		Date date = determineDate(request);
+
 		// Process Temperature/weather condition related request
 		if (action.equals(IntentConstant.WEATHER_INTENT) || action.equals(IntentConstant.WEATHER_TEMPERATURE_INTENT)) {
 			if (location != null && !location.isEmpty()) {
 				DegreeUnit degreeUnit = getDegreeUnit(request);
-				return getTemperatureResponse(location, degreeUnit);
+				return getTemperatureResponse(location, date, degreeUnit);
 			}
 		}
 		// Process Activity related intent
 		else if (action.equals(IntentConstant.WEATHER_ACTIVITY_INTENT)) {
-
+			String activity = getActivity(request);
+			if (activity != null && !activity.isEmpty()) {
+				return getActivityResponse(request, date, activity);
+			}
 		}
-
 		return "";
 	}
-	
+
 	/**
 	 * Get temperature for the particular location in requested unit (C or F)
 	 * format
@@ -67,18 +77,36 @@ public class WeatherService {
 	 * @param unit
 	 * @return
 	 */
-	private String getTemperatureResponse(String location, DegreeUnit unit) {
+	private String getTemperatureResponse(String location, Date date, DegreeUnit unit) {
 		String output = "";
+		// Yahoo weather service has this format as date
+//		SimpleDateFormat targetSdf = new SimpleDateFormat("dd MMM yyyy");
 		try {
 			yahooWeatherService.getForecastForLocation(location, unit).all().get(0).getItem().getForecasts();
 			Channel channel = yahooWeatherService.getForecastForLocation(location, unit).all().get(0);
+
 			if (channel != null) {
 				String city = channel.getLocation().getCity();
-				String condition = channel.getItem().getCondition().getText();
-				int temperature = channel.getItem().getCondition().getTemp();
-				String unitValue = channel.getUnits().getTemperature().name();
-				output = "Today in " + city + " it is " + condition + ", the temperature is " + temperature + " degree "
-						+ unitValue.toLowerCase();
+				if (DateUtils.isSameDay(date, Calendar.getInstance().getTime())) {
+					String condition = channel.getItem().getCondition().getText();
+					int temperature = channel.getItem().getCondition().getTemp();
+					String unitValue = channel.getUnits().getTemperature().name();
+					output = "Today in " + city + " it is " + condition + ", the temperature is " + temperature
+							+ " degree " + unitValue.toLowerCase();
+				} else {
+					final Calendar cal = Calendar.getInstance();
+					cal.add(Calendar.DATE, 1);
+					// If tomorrow
+					if (DateUtils.isSameDay(date, cal.getTime())) {
+						String condition = channel.getItem().getForecasts().get(1).getText();
+						int highTemp = channel.getItem().getForecasts().get(1).getHigh();
+						int lowTemp = channel.getItem().getForecasts().get(1).getLow();
+						String unitValue = channel.getUnits().getTemperature().name();
+						output = "Tomorrow in " + city + " it will be " + condition + ", the high temperature will be "
+								+ highTemp + " degree " + unitValue.toLowerCase() + "and  high temperature will be "
+								+ lowTemp + " degree " + unitValue.toLowerCase();
+					}
+				}
 			}
 		} catch (JAXBException | IOException e) {
 			log.error("Get temperature error {}", e.getMessage());
@@ -86,15 +114,19 @@ public class WeatherService {
 		return output;
 	}
 
-	
-	private String getActivityResponse(WebhookRequest request){
+	/**
+	 * Get response for weather.activity action, such as running, going out
+	 * skiing, etc.
+	 * 
+	 * @param request
+	 * @return
+	 */
+	private String getActivityResponse(WebhookRequest request, Date date, String activity) {
 		String output = "";
-		
+
 		return output;
 	}
-	
-	
-	
+
 	/**
 	 * Get degree unit as per the webhook request
 	 * 
@@ -102,7 +134,7 @@ public class WeatherService {
 	 * @return
 	 */
 	private DegreeUnit getDegreeUnit(WebhookRequest request) {
-		//Default degree unit to CELSIUS
+		// Default degree unit to CELSIUS
 		DegreeUnit unit = DegreeUnit.CELSIUS;
 		try {
 			String unitCode = request.getResult().getParameters().get("unit") != null
@@ -115,7 +147,62 @@ public class WeatherService {
 		}
 		return unit;
 	}
-	
+
+	/**
+	 * Get activity from the weather activity intent request
+	 * 
+	 * @param request
+	 * @return
+	 */
+	private String getActivity(WebhookRequest request) {
+		String activity = "";
+		try {
+			activity = (String) request.getResult().getContexts().get(0).getParameters().get("activity");
+		} catch (Exception e) {
+			log.error("Get Activity error {}", e.getMessage());
+		}
+		return activity;
+	}
+
+	/**
+	 * This method determines date string for the request. It first checks
+	 * whether response JSON object from api.ai is array or not, if not than it
+	 * parses string as date. In any failure it will return today's date as
+	 * response
+	 * 
+	 * @param request
+	 * @return
+	 */
+	private Date determineDate(WebhookRequest request) {
+		Gson gson = new Gson();
+		Date date = new Date();
+		SimpleDateFormat inputSdf = new SimpleDateFormat("yyyy-mm-dd");
+
+		try {
+			Type type = new TypeToken<List<String>>() {
+			}.getType();
+			String dateString = (String) request.getResult().getContexts().get(0).getParameters().get("date-time");
+			if (dateString != null && !dateString.isEmpty()) {
+				try {
+					List<String> dateArray = gson.fromJson(dateString, type);
+					if (dateArray != null && dateArray.size() > 0) {
+						dateString = dateArray.get(0);
+						date = inputSdf.parse(dateString);
+					}
+				} catch (Exception e) {
+					log.error("Date string is not of type array: {}", e.getMessage());
+					date = inputSdf.parse(dateString);
+				}
+			} else {
+				date = new Date();
+			}
+		} catch (Exception e) {
+			log.error("Get Date string error {}", e.getMessage());
+			date = new Date();
+		}
+		return date;
+	}
+
 	/**
 	 * This method processes webhook request and returns appropriate city to
 	 * process the request
@@ -135,7 +222,7 @@ public class WeatherService {
 				if (addressMap.size() > 0) {
 					city = addressMap.get("city");
 				}
-				//Default city - Configured
+				// Default city - Configured
 				if (city == null) {
 					city = (String) request.getResult().getContexts().get(0).getParameters().get("geo-city");
 				}
